e805fe27a08c007082af0b7b9e63b8d20e3b29c4
migueldo <mail.deoliveiramiguel@gmail.com>
2020-12-07 17:47:38 +0100

    ajout de l'ouverture d'image

---
+++ open_image.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("./logo_INSA.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	imgINSA, imgName, err := image.Decode(f)
+	fmt.Println(imgName)
+	fmt.Println(err)
+
+	blue := color.RGBA{0, 0, 255, 255}
+	for i := 100; i < 105; i++ {
+		for j := 100; j < 105; j++ {
+			imgINSA.Set(i, j, blue)
+		}
+	}
+
+	outFile, _ := os.Create("new_logo.png")
+	defer outFile.Close()
+	png.Encode(outFile, imgINSA, nil)
+}
