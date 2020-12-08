5858aedec2a43b6c8daf64d5d194edd1e6763186
migueldo <mail.deoliveiramiguel@gmail.com>
2020-12-08 18:40:56 +0100

    ca tourne mais bonnnnnn

--- noise.go
+++ noise.go
@@ -18,6 +18,8 @@ type Pixel struct {
 	A int
 }
 
+var filterMenu int = 1
+var inputFile, outputFile string
 var height, width = 0, 0
 var imgLoaded [][]Pixel
 
@@ -54,15 +56,15 @@ func encode() {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			img.Set(x, y, color.RGBA{
-				R: uint8(getRed(pixels[y][x])),
-				G: uint8(getGreen(pixels[y][x])),
-				B: uint8(getBlue(pixels[y][x])),
+				R: uint8(getRed(imgLoaded[y][x])),
+				G: uint8(getGreen(imgLoaded[y][x])),
+				B: uint8(getBlue(imgLoaded[y][x])),
 				A: 255,
 			})
 		}
 	}
 
-	f, err := os.Create("image.png")
+	f, err := os.Create(outputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -94,30 +96,40 @@ func getAlpha(lePixel Pixel) int {
 }
 
 func main() {
+	fmt.Println("Bienvenue sur notre application de filtres photo.")
 
-	file, err := os.Open("Tapping-Noise-in-Attic-at-Night.png")
+	for 1 == 1 {
+		menu()
 
-	if err != nil {
-		fmt.Println("Error: File could not be opened")
-		os.Exit(1)
+		file, err := os.Open(inputFile)
+
+		if err != nil {
+			fmt.Println("Error: File could not be opened")
+			os.Exit(1)
+		}
+
+		defer file.Close()
+
+		pixels, err := getImg(file)
+
+		if err != nil {
+			fmt.Println("Error: Image could not be decoded")
+			os.Exit(1)
+		}
+
+		switch filterMenu {
+		case 1:
+			toBlackAndWhite(pixels)
+		case 2:
+			meanBis(pixels)
+		}
+		encode()
+
+		fmt.Println("Filtre applique avec succes")
 	}
-
-	defer file.Close()
-
-	pixels, err := getImg(file)
-
-	if err != nil {
-		fmt.Println("Error: Image could not be decoded")
-		os.Exit(1)
-	}
-
-	mean(pixels)
-	toBlackAndWhite(pixels)
-	encode()
-
 }
 
-func mean(pixels Pixel) {
+func mean(pixels [][]Pixel) {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for y := 1; y < height-1; y++ {
 		for x := 1; x < width-1; x++ {
@@ -131,6 +143,25 @@ func mean(pixels Pixel) {
 	}
 }
 
+func meanBis(pixels [][]Pixel) {
+	for x := 1; x < width-2; x++ {
+		for y := 1; y < height-2; y++ {
+			fmt.Println("md1")
+			newRed := getRed(pixels[y+1][x]) + getRed(pixels[y-1][x]) + getRed(pixels[y][x-1]) + getRed(pixels[y][x+1])/4
+			fmt.Println("md2")
+			newGreen := getGreen(pixels[y+1][x]) + getGreen(pixels[y-1][x]) + getGreen(pixels[y][x-1]) + getGreen(pixels[y][x+1])/4
+			fmt.Println("md3")
+			newBlue := getBlue(pixels[y+1][x]) + getBlue(pixels[y-1][x]) + getBlue(pixels[y][x-1]) + getBlue(pixels[y][x+1])/4
+			fmt.Println("md4")
+			newAlpha := getAlpha(pixels[y+1][x]) + getAlpha(pixels[y-1][x]) + getAlpha(pixels[y][x-1]) + getAlpha(pixels[y][x+1])/4
+			fmt.Println("md5")
+			fmt.Println(x, y)
+
+			imgLoaded[y][x] = Pixel{newRed, newGreen, newBlue, newAlpha}
+		}
+	}
+}
+
 func toBlackAndWhite(img [][]Pixel) {
 	for i := 0; i < len(img); i++ {
 		for j := 0; j < len(img[i]); j++ {
@@ -140,7 +171,26 @@ func toBlackAndWhite(img [][]Pixel) {
 			newGreen := newRed
 			newBlue := newRed
 
-			imgLoaded[i][j] = Pixel{newRed, newGreen, newBlue, 255}
+			imgLoaded[i][j] = Pixel{newRed, newGreen, newBlue, getAlpha(pixel)}
 		}
 	}
+}
+
+func menu() {
+	fmt.Println("\n")
+	fmt.Println("Choisissez votre filtre :")
+	fmt.Println("1 - Noir et Blanc")
+	fmt.Println("2 - diminution du bruit")
+	fmt.Println("0 - STOP")
+	fmt.Print("-> ")
+	fmt.Scanf("%d", &filterMenu)
+
+	if filterMenu == 0 {
+		os.Exit(1)
+	}
+
+	fmt.Print("Qu'elle image voulez-vous traiter : ")
+	fmt.Scanf("%s", &inputFile)
+	fmt.Print("Donnez un nom à votre nouvelle image : ")
+	fmt.Scanf("%s", &outputFile)
 }
