0f3cc59b9c05401e6087e7409ad64a4cf39a0486
ncavalierc <72066687+ncavalierc@users.noreply.github.com>
2020-12-18 22:28:21 +0100

    Add files via upload
    correction bug bord droit
    ajout commentaires

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+//importation des modules
 import (
 	"fmt"
 	"image"
@@ -10,7 +11,9 @@ import (
 	"os"
 )
 
-//type Pixel
+//définition d'un type Pixel
+// RGBA : Rouge Vert BLeu Transparence
+// coordonnées posX posY du pixel
 type Pixel struct {
 	R    int
 	G    int
@@ -20,22 +23,28 @@ type Pixel struct {
 	posY int
 }
 
+// paramètres d'éxécution du programme
 var filter, inputFile, outputFile string
+// dimension de l'image
 var height, width = 0, 0
+// matrice de Pixel contenant l'image à traiter
 var imgLoaded [][]Pixel
 
 // crée une matrice à partir d'une image
 // récupère les valeurs RGBA de chaque pixel
 func getImg(file io.Reader) ([][]Pixel, error) {
+	// ouverture du fichier
 	img, _, err := image.Decode(file)
 
 	if err != nil {
 		return nil, err
 	}
 
+	// définition de la hauteur de la largueur de l'image à traiter
 	bounds := img.Bounds()
 	width, height = bounds.Max.X, bounds.Max.Y
 
+	// parcours en 2 dimensions
 	for x := 0; x < width; x++ {
 		var row []Pixel
 		for y := 0; y < height; y++ {
@@ -47,23 +56,29 @@ func getImg(file io.Reader) ([][]Pixel, error) {
 	return imgLoaded, nil
 }
 
-//conversion uint32 -> uint8
+
+//conversion uint32 -> uint8 pour avoir des valeurs comprises entre 0 et 255
+// 0,0,0 (noir) -> 255,255,255 (blanc)
 func rgbaToPixel(r uint32, g uint32, b uint32, a uint32, x int, y int) Pixel {
 	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257), x, y}
 }
 
+// place la valeur du pixel traité (par un channel) aux bonnes coordonnées dans une nouvelle image
 func encode(out chan Pixel, img2Encode *image.RGBA) {
-	for i := 0; i <= (height-1)*(width-1); i++ {
-		pixel := <-out
-		img2Encode.Set(pixel.posX, pixel.posY, color.RGBA{
-			R: uint8(pixel.R),
-			G: uint8(pixel.G),
-			B: uint8(pixel.B),
-			A: uint8(pixel.A),
-		})
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pixel := <-out
+			img2Encode.Set(pixel.posX, pixel.posY, color.RGBA{
+				R: uint8(pixel.R),
+				G: uint8(pixel.G),
+				B: uint8(pixel.B),
+				A: uint8(pixel.A),
+			})
+		}
 	}
 }
 
+// écriture d'une image dans un nouveau fichier
 func createFile(img2Encode *image.RGBA) {
 	f, err := os.Create(outputFile)
 	if err != nil {
@@ -80,16 +95,20 @@ func createFile(img2Encode *image.RGBA) {
 	}
 }
 
+// boucle principale
 func main() {
+	// si il n'y a pas tous les arguments, redirection vers le manuel
 	if len(os.Args) < 4 {
 		help()
 		os.Exit(0)
 	}
 
+	// on récupère les paramètres
 	filter = os.Args[1]
 	inputFile = os.Args[2]
 	outputFile = os.Args[3]
 
+	// channels pour les go routines
 	var inputChannel chan Pixel
 	var feedbackChannel chan Pixel
 
@@ -98,6 +117,7 @@ func main() {
 
 	fmt.Println("Bienvenue sur notre application de filtres photo.")
 
+	// ouverture de l'image
 	file, err := os.Open(inputFile)
 
 	if err != nil {
@@ -107,6 +127,7 @@ func main() {
 
 	defer file.Close()
 
+	// on crée une matrice de Pixel à partir de l'image
 	imgLoaded, err := getImg(file)
 	img2Encode := image.NewRGBA(image.Rect(0, 0, width, height))
 
@@ -116,27 +137,36 @@ func main() {
 	}
 
 	switch filter {
+	// filtre noir et blanc
 	case "1":
 		for nbRoutine := 0; nbRoutine < 10; nbRoutine++ {
+			// création de 10 go routines
 			go blackAndWhite(inputChannel, feedbackChannel)
 		}
 	case "2":
+	// filtre réducteur de bruit
 		for nbRoutine := 0; nbRoutine < 10; nbRoutine++ {
+			// création de 10 go routines
 			go noiseReduction(imgLoaded, inputChannel, feedbackChannel, 1)
 		}
 	}
 
+	// A DEFINIR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	go feedInput(inputChannel, imgLoaded)
 
+	// on récupère le résultat produit par une go routine
+	// enregistrement de l'image traitée
 	encode(feedbackChannel, img2Encode)
 
 	fmt.Println("Filtre applique avec succes")
 
+	// écriture de l'image traitée dans un fichier
 	createFile(img2Encode)
 
 	fmt.Println("Fichier créé avec succes")
 }
 
+// A DEFINIR !!!
 func feedInput(inp chan Pixel, pixels [][]Pixel) {
 	for cptX := 0; cptX < width; cptX++ {
 		for cptY := 0; cptY < height; cptY++ {
@@ -144,9 +174,9 @@ func feedInput(inp chan Pixel, pixels [][]Pixel) {
 			inp <- toPush
 		}
 	}
-	fmt.Printf("#DEBUG All Pushed\n")
 }
 
+// fonction réducteur de bruit
 func noiseReduction(img [][]Pixel, in chan Pixel, out chan Pixel, srdSize int) {
 	for {
 		var chgPixel Pixel
@@ -154,30 +184,42 @@ func noiseReduction(img [][]Pixel, in chan Pixel, out chan Pixel, srdSize int) {
 		pixel := <-in
 
 		switch pixel.posX {
+		// cas du bord gauche
 		case 0:
 			switch pixel.posY {
+			// cas du bord haut
 			case 0:
 				surroundMean(img, pixel, []int{0, srdSize, 0, srdSize}, &chgPixel, &cpt)
+			// cas du bord bas
 			case height - 1:
 				surroundMean(img, pixel, []int{0, srdSize, srdSize, 0}, &chgPixel, &cpt)
+			// cas "normal"
 			default:
 				surroundMean(img, pixel, []int{0, srdSize, srdSize, srdSize}, &chgPixel, &cpt)
 			}
+		// cas du bord droit
 		case width - 1:
 			switch pixel.posY {
+			// cas du bord haut
 			case 0:
 				surroundMean(img, pixel, []int{srdSize, 0, 0, srdSize}, &chgPixel, &cpt)
+			// cas du bord bas
 			case height - 1:
 				surroundMean(img, pixel, []int{srdSize, 0, srdSize, 0}, &chgPixel, &cpt)
+			// cas "normal"
 			default:
 				surroundMean(img, pixel, []int{srdSize, 0, srdSize, srdSize}, &chgPixel, &cpt)
 			}
+		// cas "normal"
 		default:
 			switch pixel.posY {
+			// cas du bord haut
 			case 0:
 				surroundMean(img, pixel, []int{srdSize, srdSize, 0, srdSize}, &chgPixel, &cpt)
+			// cas du bord bas
 			case height - 1:
 				surroundMean(img, pixel, []int{srdSize, srdSize, srdSize, 0}, &chgPixel, &cpt)
+			// cas "normal"
 			default:
 				surroundMean(img, pixel, []int{srdSize, srdSize, srdSize, srdSize}, &chgPixel, &cpt)
 			}
@@ -191,6 +233,7 @@ func noiseReduction(img [][]Pixel, in chan Pixel, out chan Pixel, srdSize int) {
 	}
 }
 
+// A DEFINIR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 func surroundMean(img [][]Pixel, pixel Pixel, srdSizes []int, chgPixel *Pixel, cpt *int) {
 	for x := pixel.posX - srdSizes[0]; x <= pixel.posX+srdSizes[1]; x++ {
 		for y := pixel.posY - srdSizes[2]; y <= pixel.posY+srdSizes[3]; y++ {
@@ -203,6 +246,7 @@ func surroundMean(img [][]Pixel, pixel Pixel, srdSizes []int, chgPixel *Pixel, c
 	}
 }
 
+// filtre noir et blanc
 func blackAndWhite(in chan Pixel, out chan Pixel) {
 	for {
 		pixel := <-in
@@ -215,6 +259,7 @@ func blackAndWhite(in chan Pixel, out chan Pixel) {
 	}
 }
 
+// appelé si le programme est exécuté sans les 3 paramètres demandés
 func help() {
 	fmt.Println("\nMANUAL\n")
 	fmt.Println("image-filter [filter-choice] [input-image] [output-image]\n")
